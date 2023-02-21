0786a248b02757b78d52a3d72712b281678e202f
HASIF <asifalikalody@gmail.com>
2023-02-21 18:34:45 +0530

    fixed issues on HeaderTop

---
+++ src/Components/RecentViews/RecentViews.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './RecentViews.css'
+function RecentViews() {
+  return (
+    <div className='rscntVwsMn'>
+      
+    </div>
+  )
+}
+
+export default RecentViews
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,6 +9,7 @@ import Section1 from '../Components/Section1/Section1'
 import Section2 from '../Components/Section2/Section2'
 import HotSale from '../Components/HotSale/HotSale'
 import Section3 from '../Components/Section3/Section3'
+import RecentViews from '../Components/RecentViews/RecentViews'
 
 function Home() {
   return (
@@ -21,8 +22,9 @@ function Home() {
       <Section2/>
       <Recommendations/>
       <FlashSale/>
-      <HotSale/> */}
-      <Section3/>
+      <HotSale/>
+      <Section3/> */}
+      <RecentViews/>
     </div>
   )
 }
